aa99a17fb3d6af812ba8c2b01e5be0589be928f4
nightink <chceyes@gmail.com>
2019-12-04 15:30:10 +0800

    fix: 修复路径后缀写死问题

--- test/un.test.ts
+++ test/un.test.ts
@@ -12,6 +12,14 @@ describe('zip_to_json', () => {
     assert(typeof buf.assets[0].p === 'string');
   });
 
+  it('zipToJSON json name not data.json success', async () => {
+    const notName = fs.createReadStream(__dirname + '/fixtures/lottie_name_not_data.zip');
+    const bufs: any = await zip2json(notName);
+    assert(bufs.length);
+    const buf = bufs[0];
+    assert(typeof buf.assets[0].p === 'string');
+  });
+
   it('zipToJSON error', async () => {
     try {
       await zip2json(fs.createReadStream(__dirname + '/fixtures/lottie_error.zip'));
